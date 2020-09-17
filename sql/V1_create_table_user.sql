18a74f93c8488fb46fabfced997a660a3faf7793
Sergei <kahu908@gmail.com>
2020-09-17 09:48:17 +0300

    Modify *.sql

--- sql/V1_create_table_user.sql
+++ sql/V1_create_table_user.sql
@@ -2,4 +2,4 @@ CREATE TABLE `db1`.`user` (
   `username` VARCHAR(16) NOT NULL,
   `email` VARCHAR(256) NULL,
   `password` VARCHAR(32) NOT NULL,
-  `create_time`  NULL DEFAULT CURRENT_TIMESTAMP);
+  `create_time` DATETIME NULL DEFAULT CURRENT_TIMESTAMP);
