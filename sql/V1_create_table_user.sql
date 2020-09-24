bed5e984cef8d0b50b5adba17ca1309abfad4429
Anvar <anvar00755@mail.ru>
2020-09-24 08:51:36 +0300

    user and teaher table migrate

--- sql/V1_create_table_user.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `db1`.`user` (
-  `username` VARCHAR(16) NOT NULL,
-  `email` VARCHAR(256) NULL,
-  `password` VARCHAR(32) NOT NULL,
-  `create_time` DATETIME NULL DEFAULT CURRENT_TIMESTAMP);
